b960c1ecc05049c6df509a77db3d4c44a3511ee7
wuque <wuquejs@163.com>
2021-03-21 16:58:40 +0800

    用于返回请求的状态信息和序列化后的对象

---
+++ src/main/java/cc/wuque/domain/ResultInfo.java
@@ -0,0 +1,48 @@
+package cc.wuque.domain;
+
+import java.io.Serializable;
+
+/**
+ * 用于返回请求的状态信息和序列化后的对象
+ * @Author 无缺
+ * @Date 2021/3/21 16:29
+ */
+public class ResultInfo implements Serializable {
+    private boolean flag;
+    private Object data;
+
+    public ResultInfo() {
+    }
+
+    private String msg;
+
+    public ResultInfo(boolean flag, Object data, String msg) {
+        this.flag = flag;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
